e909adff8edfe0e0b9c37252bbbbe589b5d06cea
rturquier <remi.turquier@ensae.fr>
2023-07-13 23:38:50 +0200

    🏷️ Improve names and docstrings (#3)

--- api.py
+++ api.py
@@ -8,9 +8,9 @@ app = FastAPI()
 data_path = Path("data_processed.json")
 income_df = pd.read_json(data_path)
 
-# give the position of someone in world income distribution : return the percentile
-@app.get("/income_positionning")
-def income_positionning(income: int):
+@app.get("/percentile")
+def get_percentile(income: int) -> int:
+    """Give the position of someone in the world income distribution"""
     percentile = (
         income_df
         .query(f"threshold <= {income}")
@@ -19,28 +19,28 @@ def income_positionning(income: int):
     )
     return percentile
 
-# give the position of someone in the world income distribution with a given amount of donation : return the percentile
-@app.get("/post_donation_position")
-def post_donation_position(income: int, donation:int):
-    r_donation = real_donation(income, donation)
-    post_donation_income = income - r_donation
-    new_income_positionning = income_positionning(post_donation_income)
-    return new_income_positionning
 
-#give the real amount of donation after tax exemption in (%)
+@app.get("/post_donation_percentile")
+def get_post_donation_percentile(income: int, donation:int):
+    """Get the post-donation position in the income distribution"""
+    post_deduction_donation = get_post_deduction_donation(income, donation)
+    post_donation_income = income - post_deduction_donation
+    post_donation_percentile = get_percentile(post_donation_income)
+    return post_donation_percentile
+
+
 @app.get("/tax_exempted_donation")
-def real_donation(income: int, donation: int):
-
+def get_post_deduction_donation(income: int, donation: int):
+    """Give the real amount of donation after tax exemption in (%)"""
     if donation < 0 or donation > 100:
-        real_donation = 0
+        post_deduction_donation = 0
     else :
         eligibility_cap = 0.20 * income
         deduction_rate = 1-0.66
         eligible_amount = min((donation/100)*income, eligibility_cap)
-        real_donation = ((eligible_amount * deduction_rate)/income)*100
-        real_donation = round(real_donation, 2)
-    return real_donation
-
+        post_deduction_donation = ((eligible_amount * deduction_rate)/income)*100
+        post_deduction_donation = round(post_deduction_donation, 2)
+    return post_deduction_donation
 
 
 if __name__ == '__main__':
